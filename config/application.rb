6976ec1dd3b60c463583f114ce124250b07306b9
Shaban Karumba <shabankarumba@gmail.com>
2013-12-09 23:19:46 +0000

    removed config for email line in application.rb file

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-CONFIG = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-CONFIG.merge! CONFIG.fetch(Rails.env, {})
-CONFIG.symbolize_keys!
+# CONFIG = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+# CONFIG.merge! CONFIG.fetch(Rails.env, {})
+# CONFIG.symbolize_keys!
 
 module BlindLove
   class Application < Rails::Application
