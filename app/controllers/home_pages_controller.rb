42f6991955316d584b369644c2f17cd0b9ffe373
Shaban Karumba <shabankarumba@gmail.com>
2014-01-04 16:56:43 +0000

    Added will_paginate

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -2,6 +2,6 @@ class HomePagesController <  ApplicationController
   skip_before_filter :authenticate_user!
 
   def index
-    @products = Product.all
+    @products = Product.paginate(:page => params[:page], :per_page => 6)
   end
 end
